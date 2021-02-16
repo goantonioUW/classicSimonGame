54784e4b1ff1bda79e7f90d8a04f2035b661e3fd
Antonio Gonzalez <goantonio31@outlook.com>
2021-02-15 17:35:40 -0800

    initial commit

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,26 @@
+// $("#green").on("click", function(){
+//     $("#green").slideUp();
+// }),
+
+// $("#blue").on("click", function(){
+//     $("#blue").slideUp().animate("margin: 15");
+// }),
+
+// $("#red").on("click", function(){
+//     $("#red").slideUp().slideDown().animate("margin: 15");
+// }),
+
+// $("#yellow").on("click", function(){
+//     $("#yellow").slideUp().slideDown().animate("margin: 15");
+// });
+
+function nextSequence() {
+    let randomNum = Math.floor(Math.random() * Math.floor());
+    return randomNum;
+}
+
+nextSequence();
+nextSequence();
+nextSequence();
+nextSequence();
+nextSequence();
