cc42ad0725ce700e4541ef97a508adc53e25af79
Antonio Gonzalez <goantonio31@outlook.com>
2021-02-16 13:40:00 -0800

    adding sounds and to randomChosenColor

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -10,5 +10,12 @@ function nextSequence() {
 
   gamePattern.push(randomChosenColour);
 
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 };
 
+// ${".btn"}.click( function () {
+//   $(this).attr()
+// })
